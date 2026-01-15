1d0cedf7988fbf36c3f82c5ff90486e98231e233
Tanmay Rathod <rathodsonu107@gmail.com>
2026-01-15 20:52:42 +0530

    validation_cjheck

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -5,6 +5,10 @@ import {ENV} from "./env.js";
 export const connectDB = async () =>
 {
     try {
+        if(!ENV.DB_URL)
+        {
+            throw new Error("DB_URL is not defined in environment variables");
+        }
         const conn = await mongoose.connect(ENV.DB_URL);
         console.log("MongoDB Connected");
     } catch(error)
