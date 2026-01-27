ae0e98e8b3888f5ab1fc1a7f99c8f877b4ef0d16
Tanmay Rathod <rathodsonu107@gmail.com>
2026-01-27 21:27:07 +0530

    new

--- frontend/src/pages/ProblemPage.jsx
+++ frontend/src/pages/ProblemPage.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { PROBLEMS } from "../data/problems";
-import Navbar from "../components/Navbar";
+import Navbar from "../components/navbar";
 
 import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
 import ProblemDescription from "../components/ProblemDescription";
