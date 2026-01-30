7eb01ff9577142c5909ad39bfbe3f0f8fa685804
Tanmay Rathod <rathodsonu107@gmail.com>
2026-01-30 19:27:22 +0530

    4

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,15 +1,8 @@
 import axios from "axios";
-import { getToken } from "@clerk/clerk-react";
 
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
 
-axiosInstance.interceptors.request.use(async (config) => {
-  const token = await window.Clerk?.session?.getToken();
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
+export default axiosInstance;
