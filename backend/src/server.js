f1ceb2bcfc6bd364fbc77ab3babc11dfd6243899
Tanmay Rathod <rathodsonu107@gmail.com>
2026-01-30 18:33:49 +0530

    new cahnges just liitlle

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,7 +17,14 @@ const __dirname = path.resolve();
 console.log(ENV.PORT);
 
 app.use(express.json());
-app.use(cors({origin : ENV.CLIENT_URL,credentials : true}));
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://talent-iq-five.vercel.app"
+  ],
+  credentials: true
+}));
+
 app.use("/api/inngest",serve({client : inngest,functions}));
 app.use(clerkMiddleware());
 app.use("/api/chat",chatRoutes);
